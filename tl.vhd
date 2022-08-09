7acda29dc03267aab1a48d09f49594b0f4acfd18
kkm213 <kkm213@korea.ac.kr>
2022-08-09 17:15:04 +0900

    upload

--- tl.vhd
+++ tl.vhd
@@ -24,14 +24,14 @@ begin
     map_l : dff port map(lin,en,rst,clk,lout);
 
     gin <= (not gout and not lout not yout and rout) or (gout and lout and not yout and not rout);
-    lin <= not gout -- and not lout and not yout and rout;
     yin <= gout and not lout and not yout and not rout;
     rin <= (not gout and not lout and not yout and not rout) or (not gout not lout and yout and not rout);
+    lin <= not gout and not lout and not yout and rout;
 
     g <= gout;
-    l <= lout;
     y <= yout;
     r <= yout;
+    l <= lout;
 
 end architecture arch_tl;
 
